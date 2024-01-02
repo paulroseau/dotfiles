fe8f886516390e8f508b8200011042c8517a1bf1
Paul Roseau <proseau@google.com>
2024-01-02 15:26:17 +0100

    Some general nvim settings (split on the right, don't show mode)

--- configs/nvim/dot-config/nvim/lua/settings.lua
+++ configs/nvim/dot-config/nvim/lua/settings.lua
@@ -4,19 +4,24 @@ vim.o.showmatch = true                     -- shows matching parenthesis
 vim.o.matchtime = 2                        -- time during which the matching parenthesis is shown
 vim.o.linebreak = true                     -- don't cut lines in the middle of a word
 vim.o.number = true                        -- show line numbers
+vim.o.showmode = false                     -- don't show the mode we are in (such as '--INSERT--')
 vim.o.numberwidth = 1                      -- minimum line number column width
 vim.opt.listchars = "tab:▸ ,eol:↲"
 vim.o.clipboard = 'unnamedplus'            -- copy/paste to/from clipboard provider (xclip, pbcopy, etc.)
 vim.o.cursorline = true                    -- highlight line cursor is currently on
-vim.opt.completeopt:append { "noinsert" }    -- select the first item of popup menu automatically without inserting it
+vim.opt.completeopt:append { "noinsert" }  -- select the first item of popup menu automatically without inserting it
 
----- Search
+-- Windows
+vim.o.splitbelow = false -- force horizontal splits to go above the current window
+vim.o.splitright = true  -- force vertical splits to go to the right of the current window
+
+-- Search
 vim.o.ignorecase = true -- case insensitive by default
 vim.o.smartcase = true  -- become case sensitive if a captial letter is typed
 vim.o.wrapscan = false  -- don't go back to first match after the last match is found.
 
----- Tab key and character handling
-vim.o.expandtab = true    -- tab transformed in spaces
-vim.o.tabstop = 2         -- tab correspond to x spaces and vice-versa depending on whether expandtab is on
-vim.o.softtabstop = 2     -- column offset when PRESSING the tab key (or the backspace key around spaces)
-vim.o.shiftwidth = 2      -- column offset when using keys '>' and '<' in normal mode
+-- Tab key and character handling
+vim.o.expandtab = true  -- tab transformed in spaces
+vim.o.tabstop = 2       -- tab correspond to x spaces and vice-versa depending on whether expandtab is on
+vim.o.softtabstop = 2   -- column offset when PRESSING the tab key (or the backspace key around spaces)
+vim.o.shiftwidth = 2    -- column offset when using keys '>' and '<' in normal mode
