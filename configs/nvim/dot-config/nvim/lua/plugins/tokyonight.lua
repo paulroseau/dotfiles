c3b8b72a35a445c759e2f401b2a5fcc39bc9bf01
Paul Roseau <proseau@google.com>
2024-01-04 13:12:23 +0100

    Update default tokyonight theme to Moon

--- configs/nvim/dot-config/nvim/lua/plugins/tokyonight.lua
+++ configs/nvim/dot-config/nvim/lua/plugins/tokyonight.lua
@@ -1,5 +1,5 @@
 require("tokyonight").setup({
-  style = "night", -- `storm`, `moon`, `night` and `day`
+  style = "moon", -- `storm`, `moon`, `night` and `day`
   light_style = "day", -- theme used when the background is set to light
   transparent = false,
 })
