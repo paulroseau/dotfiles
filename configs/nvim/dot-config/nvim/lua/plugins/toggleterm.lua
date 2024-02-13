b23220ac089e812b16ba0541b756f9724848b74c
Paul Roseau <proseau@google.com>
2024-02-13 18:20:31 +0100

    Map 'q' to 'i' for toggleterm windows (mimic tmux a bit here)

--- configs/nvim/dot-config/nvim/lua/plugins/toggleterm.lua
+++ configs/nvim/dot-config/nvim/lua/plugins/toggleterm.lua
@@ -1,5 +1,12 @@
-require("toggleterm").setup({
+require('toggleterm').setup({
   open_mapping = '<leader>t',
-  shell = "zsh",
-  direction = "float"
+  shell = 'zsh',
+  direction = 'float'
+})
+
+vim.api.nvim_create_autocmd('TermOpen', { 
+  pattern = 'term://*toggleterm#*',
+  callback = function(args)
+    vim.keymap.set('', 'q', 'i', { buffer = args.buf })
+  end
 })
