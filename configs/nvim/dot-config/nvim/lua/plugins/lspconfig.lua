d2d4ecfcd803dce3ff52eaf497ac36ca6a98743a
Paul Roseau <proseau@google.com>
2024-03-20 11:10:26 +0100

    Don't add all the rtp paths to the library workspace for the lua lsp, just the builtin VIMRUNTIME

--- configs/nvim/dot-config/nvim/lua/plugins/lspconfig.lua
+++ configs/nvim/dot-config/nvim/lua/plugins/lspconfig.lua
@@ -1,8 +1,22 @@
 local lspconfig = require('lspconfig')
 
-lspconfig.clangd.setup({})
+-- From README of https://github.com/hrsh7th/cmp-nvim-lsp
+-- nvim-cmp supports more types of completion candidates, so users must override
+-- the LSP client's capabilities sent to the server such that it can provide
+-- these candidates during a completion request.
+-- lspconfig initializes clients with a default config based of
+-- lsp.protocol.make_client_capabilities() (cf. lspconfig/util.lua) and
+-- merges it with what we pass in arguments (cf. use of
+-- vim.tbl_deep_extend('keep', ...))
+local lsp_client_extra_capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+lspconfig.clangd.setup({
+  capabilities = lsp_client_extra_capabilities,
+})
 
 lspconfig.lua_ls.setup({
+  capabilities = lsp_client_extra_capabilities,
+
   -- Update the client settings if no file configuring the LSP server to include
   -- all RTP paths in the context
   -- We update the client.config.setting so those changes are sent to the LSP
@@ -11,13 +25,14 @@ lspconfig.lua_ls.setup({
   -- We are using on_init to be able to look up the client and find the path of the workspace folder
   -- where the configuration might be, otherwise a simpler approach would be to
   -- just pass the settings parameter directly to the setup function
+  
   on_init = function(client)
     local path = client.workspace_folders[1].name
     if not vim.loop.fs_stat(path .. '/.luarc.json') and not vim.loop.fs_stat(path .. '/.luarc.jsonc') then
       client.config.settings = {
         Lua = {
           workspace = {
-            library = vim.api.nvim_get_runtime_file("", true)
+            library = { vim.env.VIMRUNTIME },
           }
         }
       }
