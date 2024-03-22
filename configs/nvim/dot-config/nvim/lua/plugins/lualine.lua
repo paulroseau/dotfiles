ae78f90546155c26b22376333d9a51953dfaffb7
Paul Roseau <proseau@google.com>
2024-03-22 09:52:02 +0100

    Configure lualine

--- configs/nvim/dot-config/nvim/lua/plugins/lualine.lua
+++ configs/nvim/dot-config/nvim/lua/plugins/lualine.lua
@@ -1,1 +1,14 @@
-require('lualine').setup()
+require('lualine').setup({
+  options = {
+    component_separators = { left = '╲', right = '╱'},
+    section_separators = { left = '', right = ''},
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {''},
+    lualine_y = {'filetype'},
+    lualine_z = {'progress'},
+  },
+})
