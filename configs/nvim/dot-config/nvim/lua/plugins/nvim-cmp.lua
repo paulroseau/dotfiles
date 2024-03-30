81064e2c38e1f69ce356b5d4e0294e18982e68ba
Paul Roseau <proseau@google.com>
2024-03-30 17:24:38 +0100

    Add nvim-cmp doc scrolling mappings

--- configs/nvim/dot-config/nvim/lua/plugins/nvim-cmp.lua
+++ configs/nvim/dot-config/nvim/lua/plugins/nvim-cmp.lua
@@ -10,7 +10,20 @@ local complete_if_invisible = function(callback)
   end
 end
 
+local toggle_docs = function(_)
+  if cmp.visible_docs() then
+    cmp.close_docs()
+  else
+    cmp.open_docs()
+  end
+end
+
 cmp.setup({
+  view = {
+    docs = {
+      auto_open = false
+    }
+  },
   snippet = {
     expand = function(args)
       require('luasnip').lsp_expand(args.body)
@@ -33,6 +46,18 @@ cmp.setup({
       i = cmp.mapping.confirm({ select = true }),
       c = cmp.mapping.confirm({ select = false }),
     }),
+    ['<M-p>'] = cmp.mapping(
+      toggle_docs, 
+      { 'i', 'c' }
+    ),
+    ['<M-d>'] = cmp.mapping(
+      function() cmp.scroll_docs(5) end,
+      { 'i', 'c' }
+    ),
+    ['<M-e>'] = cmp.mapping(
+      function() cmp.scroll_docs(-5) end,
+      { 'i', 'c' }
+    ),
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp', keyword_length = 3 },
