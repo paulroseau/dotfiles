324c7b0b7e29c16cf53e515da8353361a77c5e5f
Paul Roseau <proseau@google.com>
2024-04-26 00:33:33 +0200

    Override rust-analyzer such that we can navigate rust std library source with it (rust-analyzer would fail on it because Cargo.lock were missing)

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -51,5 +51,36 @@ in
       };
     };
 
+    # rust-analyzer tries to generate the Cargo.lock file for a project when it
+    # is missing. Since the standard library source code does not contain any
+    # Cargo.lock files, rust-analyzer crashes because it does not have write
+    # permissions on /nix/store/...
+    # Here we create a copy of the rustLibSrc and override this parameter when
+    # deriving rust-analyzer. We don't use an overlay on rustPlatform.rustLibSrc
+    # because that causes nix to rebuild every rust package rather than pulling
+    # those from the cache
+    rust-analyzer = super.rust-analyzer.override {
+      rustSrc = super.runCommand "rust-lib-src-with-cargo-lock" {
+         # necessary to make this a fixed output derivation so the build can run
+         # outside of the sandbox since Cargo needs to fetch dependencies over
+         # the network
+         outputHashMode = "recursive";
+         outputHashAlgo = "sha256";
+         outputHash = "sha256-28u+68aY8n/uU/vvZZuyN/pPpTQIG+Tb97IFxQvgcis="; 
+       } ''
+         mkdir $out
+         cp -r ${super.rustPlatform.rustLibSrc}/* $out/
+         chmod -R u+w $out/
+
+         # necessary so Cargo can validate the SSL certs from crates.io
+         export CARGO_HTTP_CAINFO=${super.cacert}/etc/ssl/certs/ca-bundle.crt
+
+         # necessary for Cargo to find crates included in the directory tree
+         cd $out
+
+         find . -name 'Cargo.toml' -exec ${super.cargo}/bin/cargo generate-lockfile --manifest-path {} \;
+       '';
+    };
+
     myPkgs = import ./make-bundles.nix self;
   }
