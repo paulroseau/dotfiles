274dba9823687f0ea207e4f3553c14ce5149aede
Paul Roseau <proseau@google.com>
2024-06-27 00:25:59 +0200

    Neotree mappings: toogle with ,x  / force focus ,X

--- configs/nvim/dot-config/nvim/lua/plugins/neo-tree.lua
+++ configs/nvim/dot-config/nvim/lua/plugins/neo-tree.lua
@@ -54,5 +54,5 @@ require("neo-tree").setup({
   },
 })
 
-vim.keymap.set({'n'} , '<leader>x', '<cmd>Neotree reveal toggle<CR>')
-vim.keymap.set({'n'} , '<leader>X', '<cmd>Neotree reveal<CR>')
+vim.keymap.set({'n'} , '<leader>x', '<cmd>Neotree reveal toggle show<CR>')
+vim.keymap.set({'n'} , '<leader>X', '<cmd>Neotree reveal_force_cwd focus<CR>')
