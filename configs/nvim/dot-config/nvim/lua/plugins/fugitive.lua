1adb3dba8af3710d49ee0af68138d7012ed2250b
Paul Roseau <proseau@google.com>
2024-07-04 17:17:00 +0200

    Fugitive: Add mapping for Git blame

--- configs/nvim/dot-config/nvim/lua/plugins/fugitive.lua
+++ configs/nvim/dot-config/nvim/lua/plugins/fugitive.lua
@@ -1,3 +1,4 @@
 vim.keymap.set({'n'} , '<leader>gg', '<cmd>Git<CR>')
 vim.keymap.set({'n'} , '<leader>gd', '<cmd>Gvdiffsplit<CR>')
 vim.keymap.set({'n'} , '<leader>gl', '<cmd>GcLog %<CR>')
+vim.keymap.set({'n'} , '<leader>gb', '<cmd>Git blame<CR>')
