3f585030cfa784aba4096347a206690226635a36
Paul Roseau <proseau@google.com>
2024-10-03 12:15:38 +0200

    Add nord.nvim colorschemes, and update colorschemes plugin organization

---
+++ configs/nvim/dot-config/nvim/lua/plugins/colorschemes/nord.lua
@@ -0,0 +1,8 @@
+vim.g.nord_contrast = false
+vim.g.nord_borders = true
+vim.g.nord_disable_background = false
+vim.g.nord_cursorline_transparent = false
+vim.g.nord_enable_sidebar_background = false
+vim.g.nord_italic = true
+vim.g.nord_uniform_diff_background = false
+vim.g.nord_bold = true
