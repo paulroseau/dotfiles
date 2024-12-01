a1c32fa1bdb1e8d12fdd16df1cfb7907af169da5
Paul Roseau <proseau@google.com>
2024-12-01 17:45:07 +0100

    colorschemes: tokyonight instead of onedark

--- configs/nvim/dot-config/nvim/lua/plugins/colorschemes/onedark.lua
+++ configs/nvim/dot-config/nvim/lua/plugins/colorschemes/onedark.lua
@@ -1,3 +1,3 @@
 require('onedark').setup {
-  style = 'deep' -- options are 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer'
+  style = 'cool', -- options are 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer'
 }
--- configs/nvim/dot-config/nvim/lua/plugins/colorschemes/tokyonight.lua
+++ configs/nvim/dot-config/nvim/lua/plugins/colorschemes/tokyonight.lua
@@ -2,4 +2,13 @@ require("tokyonight").setup({
   style = "moon", -- `storm`, `moon`, `night` and `day`
   light_style = "day", -- theme used when the background is set to light
   transparent = false,
+  keywords = { italic = true },
+
+  on_colors = function(colors)
+    colors.fg = "#a8aecb" -- default is "too white"
+  end,
+
+  on_highlights = function(highlights, colors)
+    highlights["@module"] = { link = "Identifier" } -- default is Include which makes imports hard to see since imports also links to Include
+  end,
 })
