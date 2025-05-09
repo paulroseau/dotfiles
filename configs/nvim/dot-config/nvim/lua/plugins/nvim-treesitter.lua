2b75058a09fe8bfd486f219421e76517a777f64e
Paul Roseau <polo@Polos-MacBook-Air.local>
2025-05-09 10:05:27 +0200

    nvim-treesitter: install parsers automatically if not installed through nix

--- configs/nvim/dot-config/nvim/lua/plugins/nvim-treesitter.lua
+++ configs/nvim/dot-config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,7 +1,37 @@
-require('nvim-treesitter.configs').setup {
-  -- Don't install anything through nvim-treesitter plugin,
-  -- parsers are compiled and added to the rtp with nix
-  ensure_installed = {},
+local nvim_treesitter_configs = require('nvim-treesitter.configs')
+
+local ensure_installed = {
+  "bash", 
+  "c", 
+  "dockerfile",
+  "go",
+  "haskell",
+  "hcl",
+  "json",
+  "lua",
+  "markdown",
+  "nix",
+  "ocaml",
+  "python",
+  "query",
+  "rust",
+  "scala",
+  "vim",
+  "vimdoc",
+  "yaml",
+  -- "zsh"  not supported by nvim-treesitter yet, even though the grammar is available https://github.com/tree-sitter-grammars/tree-sitter-zsh
+}
+
+local nvim_treesitter_source = debug.getinfo(nvim_treesitter_configs.setup).source
+if string.find(nvim_treesitter_source, ".nix-profile/", 1, true) then
+  -- Setting ensure_installed to a non nil value causes 
+  -- nvim-treesitter to first check that the parser_install_dir is
+  -- writable, which is not the case when installed through nix...
+  ensure_installed = nil
+end
+
+nvim_treesitter_configs.setup {
+  ensure_installed = ensure_installed,
   auto_install = false,
 
   highlight = {
