b30f6c5edb9a43984e0caf180e440de238e488d7
Paul Roseau <polo@Polos-MacBook-Air.local>
2025-08-18 09:42:54 +0200

    🎨  formatting

--- configs/nvim/dot-config/nvim/lua/plugins/nvim-resize-window.lua
+++ configs/nvim/dot-config/nvim/lua/plugins/nvim-resize-window.lua
@@ -2,7 +2,7 @@ local resize_window = require("nvim-resize-window")
 
 local increment = 3
 
-vim.keymap.set({'', 't'} , '<M-<>', resize_window.left(increment))
-vim.keymap.set({'', 't'} , '<M-+>', resize_window.down(increment)) 
-vim.keymap.set({'', 't'} , '<M-->', resize_window.up(increment))
-vim.keymap.set({'', 't'} , '<M->>', resize_window.right(increment))
+vim.keymap.set({ '', 't' }, '<M-<>', resize_window.left(increment))
+vim.keymap.set({ '', 't' }, '<M-+>', resize_window.down(increment))
+vim.keymap.set({ '', 't' }, '<M-->', resize_window.up(increment))
+vim.keymap.set({ '', 't' }, '<M->>', resize_window.right(increment))
