832f7a0922faef97f68b68776518b7cdbc18dff7
Paul Roseau <polo@Host-001.lan>
2025-09-17 21:44:48 +0200

    ✨ install solarized & code companion

---
+++ configs/nvim/dot-config/nvim/lua/plugins/colorschemes/solarized.lua
@@ -0,0 +1,8 @@
+require('solarized').setup({
+  palette = 'solarized', -- solarized (default) | selenized
+  variant = 'autumn',    -- "spring" | "summer" | "autumn" | "winter" (default)
+  plugins = {
+    treesitter = true,
+    neotree = true,
+  },
+})
