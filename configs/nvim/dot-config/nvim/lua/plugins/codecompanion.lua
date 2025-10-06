31a042d65c0c4dcb1135b30bea6423e010d6141d
Paul Roseau <polo@Polos-MacBook-Air.local>
2025-10-06 17:17:02 +0200

    🚧 get started with codecompanion

--- configs/nvim/dot-config/nvim/lua/plugins/codecompanion.lua
+++ configs/nvim/dot-config/nvim/lua/plugins/codecompanion.lua
@@ -1,1 +1,27 @@
-require('codecompanion').setup()
+require('codecompanion').setup({
+  display = {
+    action_palette = {
+      provider = 'fzf_lua',
+    }
+  },
+
+  strategies = {
+    chat = { adapter = 'openai' },
+    inline = { adapter = 'openai' },
+  },
+
+  adapters = {
+    http = {
+      openai = function()
+        return require('codecompanion.adapters').extend('openai', {
+          schema = {
+            model = {
+              default = 'gpt-5',
+            }
+          }
+
+        })
+      end,
+    }
+  },
+})
