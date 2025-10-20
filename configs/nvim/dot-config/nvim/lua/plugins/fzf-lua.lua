dc7249e2207a631d457b41d108e9464d3480c156
Paul Roseau <polo@Polos-MacBook-Air.local>
2025-10-20 08:16:58 +0200

    🩹 fix fzf-lua delete buffers when No Name

--- configs/nvim/dot-config/nvim/lua/plugins/fzf-lua.lua
+++ configs/nvim/dot-config/nvim/lua/plugins/fzf-lua.lua
@@ -1,11 +1,12 @@
-local fzf = require("fzf-lua")
+local fzf = require('fzf-lua')
 
 -- Adapted from fzf-lua.actions to prevent blocking deletions of dirty
 -- buffers which have not been written to disk
 local buf_del = function(selected, opts)
   for _, sel in ipairs(selected) do
     local entry = fzf.path.entry_to_file(sel, opts)
-    if entry.bufnr and (entry.path == "[No Name]" or not fzf.utils.buffer_is_dirty(entry.bufnr, true, false)) then
+    vim.print(entry.path)
+    if entry.bufnr and (vim.endswith(entry.path, '[No Name]') or not fzf.utils.buffer_is_dirty(entry.bufnr, true, false)) then
       vim.api.nvim_buf_delete(entry.bufnr, { force = true })
     end
   end
@@ -17,22 +18,22 @@ fzf.setup({
     -- These override the default tables completely so we need to include
     -- everything we need, including what we don't want to modify
     builtin = {
-      ["<M-p>"] = "toggle-preview",
-      ["<M-r>"] = "toggle-preview-cw",
-      ["<M-w>"] = "toggle-preview-wrap",
-      ["<M-e>"] = "preview-page-up",
-      ["<M-d>"] = "preview-page-down",
+      ['<M-p>'] = 'toggle-preview',
+      ['<M-r>'] = 'toggle-preview-cw',
+      ['<M-w>'] = 'toggle-preview-wrap',
+      ['<M-e>'] = 'preview-page-up',
+      ['<M-d>'] = 'preview-page-down',
     },
     fzf = {
-      ["ctrl-z"] = "abort",
-      ["ctrl-u"] = "unix-line-discard",
-      ["ctrl-a"] = "beginning-of-line",
-      ["ctrl-e"] = "end-of-line",
-      ["alt-a"] = "toggle-all",
-      ["alt-p"] = "toggle-preview",
-      ["alt-w"] = "toggle-preview-wrap",
-      ["alt-e"] = "preview-page-up",
-      ["alt-d"] = "preview-page-down",
+      ['ctrl-z'] = 'abort',
+      ['ctrl-u'] = 'unix-line-discard',
+      ['ctrl-a'] = 'beginning-of-line',
+      ['ctrl-e'] = 'end-of-line',
+      ['alt-a'] = 'toggle-all',
+      ['alt-p'] = 'toggle-preview',
+      ['alt-w'] = 'toggle-preview-wrap',
+      ['alt-e'] = 'preview-page-up',
+      ['alt-d'] = 'preview-page-down',
     },
   },
   actions = {
@@ -40,63 +41,63 @@ fzf.setup({
     -- Here we are overriding the plugin defaults for these default actions
     -- More details with :help fzf-lua-default-options
     files = {
-      ["enter"] = fzf.actions.file_edit_or_qf,
-      ["alt-s"] = fzf.actions.file_split,
-      ["alt-v"] = fzf.actions.file_vsplit,
-      ["alt-t"] = fzf.actions.file_tabedit,
-      ["alt-q"] = fzf.actions.file_sel_to_qf,
-      ["alt-l"] = fzf.actions.file_sel_to_ll,
+      ['enter'] = fzf.actions.file_edit_or_qf,
+      ['alt-s'] = fzf.actions.file_split,
+      ['alt-v'] = fzf.actions.file_vsplit,
+      ['alt-t'] = fzf.actions.file_tabedit,
+      ['alt-q'] = fzf.actions.file_sel_to_qf,
+      ['alt-l'] = fzf.actions.file_sel_to_ll,
     },
   },
   buffers = {
     actions = {
-      ["ctrl-x"] = { fn = buf_del, reload = true },
+      ['ctrl-x'] = { fn = buf_del, reload = true },
     }
   },
   commands = {
     actions = {
-      ["enter"] = fzf.actions.ex_run_cr,
+      ['enter'] = fzf.actions.ex_run_cr,
     },
   },
   tabs = {
     actions = {
-      ["ctrl-x"] = { fn = buf_del, reload = true },
+      ['ctrl-x'] = { fn = buf_del, reload = true },
     }
   },
   helptags = {
     actions = {
-      ["alt-s"] = fzf.actions.help,
-      ["alt-v"] = fzf.actions.help_vert,
-      ["alt-t"] = fzf.actions.help_tab,
+      ['alt-s'] = fzf.actions.help,
+      ['alt-v'] = fzf.actions.help_vert,
+      ['alt-t'] = fzf.actions.help_tab,
     },
   },
   keymaps = {
     actions = {
-      ["alt-s"] = fzf.actions.keymap_split,
-      ["alt-v"] = fzf.actions.keymap_vsplit,
-      ["alt-t"] = fzf.actions.keymap_tabedit,
+      ['alt-s'] = fzf.actions.keymap_split,
+      ['alt-v'] = fzf.actions.keymap_vsplit,
+      ['alt-t'] = fzf.actions.keymap_tabedit,
     },
   },
   diagnostics = {
     copen = function()
-      vim.cmd("copen")
-      vim.cmd(".cc")
+      vim.cmd('copen')
+      vim.cmd('.cc')
     end,
   },
   git = {
     bcommits = {
       actions = {
-        ["alt-s"] = fzf.actions.git_buf_split,
-        ["alt-v"] = fzf.actions.git_buf_vsplit,
-        ["alt-t"] = fzf.actions.git_buf_tabedit,
+        ['alt-s'] = fzf.actions.git_buf_split,
+        ['alt-v'] = fzf.actions.git_buf_vsplit,
+        ['alt-t'] = fzf.actions.git_buf_tabedit,
       },
     },
   },
   manpages = {
     actions = {
-      ["alt-s"] = fzf.actions.man,
-      ["alt-v"] = fzf.actions.man_vert,
-      ["alt-t"] = fzf.actions.man_tab,
+      ['alt-s'] = fzf.actions.man,
+      ['alt-v'] = fzf.actions.man_vert,
+      ['alt-t'] = fzf.actions.man_tab,
     },
   }
 })
@@ -105,7 +106,7 @@ local function wrap_vertical(fn)
   local vertial_layout_opts = {
     winopts = {
       preview = {
-        layout = "vertical"
+        layout = 'vertical'
       }
     }
   }
