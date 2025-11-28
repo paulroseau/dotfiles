d5da78acd704725e93fd536b051f165d27bb1d89
Paul Roseau <polo@Polos-MacBook-Air.local>
2025-11-28 16:03:38 +0100

    🎨 update shell rc files + install script

--- install.sh
+++ install.sh
@@ -1,8 +1,16 @@
-# Pre-requisites:
-# - nix / nix-env / nix-shell
+#!/bin/bash
 
-clone_this_repository ()
-{
+NIX_PROFILE="$HOME/.nix-profile"
+
+# Update PATH through nix-env if present
+[ -r $NIX_PROFILE/etc/profile.d/nix.sh ] && . $NIX_PROFILE/etc/profile.d/nix.sh
+
+if ! command -v nix-env >/dev/null 2>&1; then
+  echo "Error: nix-env not found in PATH, install Nix." >&2
+  exit 1
+fi
+
+clone_this_repository () {
   nix-shell -p git --command "git clone https://github.com/paulroseau/dotfiles $HOME/.dotfiles"
 }
 
@@ -17,40 +25,4 @@ create_config_symlinks () {
 
 clone_this_repository
 create_config_symlinks
-
-# TODO clean what is below up
-
-# Install applications
-# TODO ask if you are installing on a remote workstation or locally (to install Alacritty)
-nix-env -if nix/default.nix -A myPkgs.local
-
-# link fonts here (this is for wezterm, to not have to distinguish between Linux ~/.fonts and MacOS, this could probably be done better)
-ln -s $HOME/.nix-profile/share/fonts .
-
-
-# Basic install script for Debian only
-
-# Alacritty install
-sudo ln -s $HOME/.nix-profile/share/applications/Alacritty.desktop /usr/local/share/applications/
-sudo ln -s $HOME/.nix-profile/share/icons/hicolor/scalable/apps/Alacritty.svg /usr/share/pixmaps/
-
-# Optional (if still using Alacritty) which needs fonts installed on the system
-ln -s $HOME/.nix-profile/fonts $HOME/.fonts # Note: in https://www.youtube.com/watch?v=fR4ThXzhQYI the guy puts them in .local/share/fonts
-rm $HOME/.cache/fontconfig/* # for good measure, spent hours understanding why fonts were not loaded
-
-# For Mac OS
-
-# We use -L with cp below because MacOS does not follow sym links for these funcionalities
-# A better approach could resort on using `rsync` with the options that nix-darwin uses https://github.com/nix-community/home-manager/issues/1341#issuecomment-2748323255
-
-# Make applications available from launchpad and spotlight
-cp -rL ~/.nix-profile/Applications/* ~/Applications/
-# TODO you need to use rsync for the following (replace existing target but don't mach source)
-rsync --unsafe-link-copy ~/.nix-profile/Library ~/Library
-
-# Optional (if still using Alacritty) which needs fonts installed on the system
-# Add NerdFonts (you may need to restart for them to appear)
-cp -Lr ~/.nix-profile/share/fonts/* ~/Library/Fonts/
-
-# Go to Privacy & Security settings, Full Disk Access, tick Alacritty (otherwisse you can't ls in ~/Documents, ~/Downloads with GNU ls)
-# Install Karabiner
+nix-env -f '<nixpkgs>' --install --attr bundle.base
