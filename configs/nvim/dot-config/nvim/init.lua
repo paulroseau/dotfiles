dc3783fb436aba66a63c3b92060bb29771656de6
Paul Roseau <polo@Host-001.lan>
2026-01-07 07:04:47 +0100

    🎨 sort plugins in init.lua

--- configs/nvim/dot-config/nvim/init.lua
+++ configs/nvim/dot-config/nvim/init.lua
@@ -5,30 +5,31 @@ require('commands')
 require('treesitter')
 require('lsp')
 
+-- Add personal (vendored) plugins to RTP
 local utils = require('utils')
 utils.add_child_directories_to_rtp(vim.fs.normalize(vim.fn.stdpath('config') .. '/my-plugins'))
 
 -- Configure plugins
-require('plugins.nvim-tmux')
-require('plugins.nvim-wezterm')
-require('plugins.nvim-resize-window')
+require('plugins.blink-cmp')
 require('plugins.codecompanion')
 require('plugins.comment')
 require('plugins.flatten')
 require('plugins.fugitive')
 require('plugins.fzf-lua')
-require('plugins.lualine')
-require('plugins.neo-tree')
-require('plugins.nvim-surround')
-require('plugins.toggleterm')
-require('plugins.blink-cmp')
 require('plugins.lazydev')
+require('plugins.lualine')
 require('plugins.luatab')
-require('plugins.rustaceanvim')
 require('plugins.minipairs')
+require('plugins.neo-tree')
+require('plugins.nvim-resize-window')
+require('plugins.nvim-surround')
+require('plugins.nvim-tmux')
+require('plugins.nvim-wezterm')
+require('plugins.rustaceanvim')
+require('plugins.toggleterm')
 require('plugins.zen-mode')
 
--- nvim-treesitter needs to be on the RTP if TS parsers are not managed by nix
+-- nvim-treesitter needs to be loaded if TS parsers are not managed by nix
 if pcall(require, 'nvim-treesitter') then
   require('plugins.nvim-treesitter')
 end
