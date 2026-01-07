1de428877c4fdb2980c07db7d00e7d6bf5768b85
Paul Roseau <polo@Host-001.lan>
2026-01-07 07:08:37 +0100

    ✨ add render-markdown

---
+++ configs/nvim/dot-config/nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,11 @@
+require('render-markdown').setup({
+  completions = {
+    lsp = { enabled = true }
+  },
+  anti_conceal = {
+    enabled = false, -- disable hiding added text on the line the cursor is on
+  },
+  sign = {
+    enabled = false, -- no signs in the margin (avoids too much change)
+  }
+})
