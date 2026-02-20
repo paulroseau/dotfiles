e9fc7c907575853a58d382c7064dabc93bc67a78
Paul Roseau <polo@Polos-MacBook-Air.local>
2026-02-20 09:45:17 +0100

    ✨ nicer :terminal command

--- configs/nvim/dot-config/nvim/lua/autocommands.lua
+++ configs/nvim/dot-config/nvim/lua/autocommands.lua
@@ -1,13 +1,31 @@
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'fzf',
+  callback = function(args)
+    vim.keymap.set('t', '<M-s>', '<M-s>', { buffer = args.buf })
+    vim.keymap.set('t', '<M-v>', '<M-v>', { buffer = args.buf })
+  end,
+})
+
 vim.api.nvim_create_autocmd('TermOpen', {
   callback = function(args)
     vim.keymap.set('', 'q', 'i', { buffer = args.buf })
   end,
 })
 
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = 'fzf',
+vim.api.nvim_create_autocmd({ 'TermOpen', 'BufEnter' }, {
+  pattern = 'term://*',
+  callback = function(_)
+    vim.cmd('startinsert')
+  end,
+})
+
+vim.api.nvim_create_autocmd('TermClose', {
   callback = function(args)
-    vim.keymap.set('t', '<M-s>', '<M-s>', { buffer = args.buf })
-    vim.keymap.set('t', '<M-v>', '<M-v>', { buffer = args.buf })
+    -- small defer so Neovim finishes cleaning up the terminal
+    vim.schedule(function()
+      if vim.api.nvim_buf_is_valid(args.buf) then
+        vim.api.nvim_buf_delete(args.buf, { force = true })
+      end
+    end)
   end,
 })
