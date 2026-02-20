9f43374a985a1c7452afcba6712f6e9a3b433693
Paul Roseau <polo@Polos-MacBook-Air.local>
2026-02-20 09:22:51 +0100

    🎨 allow to pass count to your custom resize window

--- configs/nvim/dot-config/nvim/lua/plugins/nvim-resize-window.lua
+++ configs/nvim/dot-config/nvim/lua/plugins/nvim-resize-window.lua
@@ -1,8 +1,15 @@
 local resize_window = require('nvim-resize-window')
 
-local increment = 3
+local default_increment = 3
 
-vim.keymap.set({ '', 't' }, '<M-<>', resize_window.left(increment))
-vim.keymap.set({ '', 't' }, '<M-+>', resize_window.down(increment))
-vim.keymap.set({ '', 't' }, '<M-->', resize_window.up(increment))
-vim.keymap.set({ '', 't' }, '<M->>', resize_window.right(increment))
+local function with_count(resize_function)
+  return function()
+    local count = vim.v.count > 0 and vim.v.count or default_increment
+    resize_function(count)()
+  end
+end
+
+vim.keymap.set({ '', 't' }, '<M-<>', with_count(resize_window.left))
+vim.keymap.set({ '', 't' }, '<M-+>', with_count(resize_window.down))
+vim.keymap.set({ '', 't' }, '<M-->', with_count(resize_window.up))
+vim.keymap.set({ '', 't' }, '<M->>', with_count(resize_window.right))
