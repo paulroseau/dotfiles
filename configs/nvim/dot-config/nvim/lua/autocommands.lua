e8631e162c18e3acd509c3f35d3e403bcc65c63a
Paul Roseau <polo@Host-001.lan>
2026-02-27 22:01:40 +0100

    🚸 remember mode before leaving a terminal window

--- configs/nvim/dot-config/nvim/lua/autocommands.lua
+++ configs/nvim/dot-config/nvim/lua/autocommands.lua
@@ -7,20 +7,24 @@ vim.api.nvim_create_autocmd('FileType', {
   end,
 })
 
+local term_group = vim.api.nvim_create_augroup('TerminalConfig', { clear = true })
+
 vim.api.nvim_create_autocmd('TermOpen', {
+  group = term_group,
   callback = function(args)
     vim.keymap.set('', 'q', 'i', { buffer = args.buf })
   end,
 })
 
-vim.api.nvim_create_autocmd({ 'TermOpen', 'BufEnter' }, {
-  pattern = 'term://*',
+vim.api.nvim_create_autocmd('TermOpen', {
+  group = term_group,
   callback = function(_)
     vim.cmd('startinsert')
   end,
 })
 
 vim.api.nvim_create_autocmd('TermClose', {
+  group = term_group,
   callback = function(args)
     -- small defer so Neovim finishes cleaning up the terminal
     vim.schedule(function()
@@ -28,5 +32,26 @@ vim.api.nvim_create_autocmd('TermClose', {
         vim.api.nvim_buf_delete(args.buf, { force = true })
       end
     end)
+  end,
+})
+
+vim.api.nvim_create_autocmd('BufEnter', {
+  group = term_group,
+  pattern = 'term://*',
+  callback = function(args)
+    local saved_mode = vim.b[args.buf].term_mode
+    if saved_mode == 't' or saved_mode == 'i' then
+      vim.cmd('startinsert')
+    end
+    -- If saved_mode was 'n' (normal mode) or nil (first time), stay in normal mode
+  end,
+})
+
+vim.api.nvim_create_autocmd('BufLeave', {
+  group = term_group,
+  pattern = 'term://*',
+  callback = function(args)
+    local mode = vim.api.nvim_get_mode().mode
+    vim.b[args.buf].term_mode = mode
   end,
 })
